25f46bc09ef3e3c6ce04bb730aa4b326b2ecff11
C.U <441389+krim404@users.noreply.github.com>
2024-02-16 23:54:01 +0100

    Update to pipe the data directly to printer.py

--- text.py
+++ text.py
@@ -1,19 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 import textwrap
 import subprocess
 import sys
 import os
 import math
 
+
 def draw_text(text, font_path, font_size=20, line_spacing=5, text_width=50):
     font = ImageFont.truetype(font_path, font_size)
     total_height = 0
     image_width = 0
-    ergebnis = []
-    zeilen = text.split('\n')
+    res = []
+    zlines = text.split('\n')
 
-    for zeile in zeilen:
-        lines = textwrap.wrap(zeile, text_width)
+    for zline in zlines:
+        lines = textwrap.wrap(zline, text_width)
 
         # Calculate est size of the image
         for line in lines:
@@ -22,7 +24,7 @@ def draw_text(text, font_path, font_size=20, line_spacing=5, text_width=50):
             if line_width > image_width:
                 image_width = line_width
             total_height += line_height + line_spacing
-            ergebnis.append(line)
+            res.append(line)
 
     total_height -= line_spacing - 4
 
@@ -32,7 +34,7 @@ def draw_text(text, font_path, font_size=20, line_spacing=5, text_width=50):
 
     # draw text line by line
     y = 0
-    for line in ergebnis:
+    for line in res:
         draw.text((0, y), line, fill="black", font=font)
         y += font.getbbox(line)[3] + line_spacing
 
@@ -40,20 +42,36 @@ def draw_text(text, font_path, font_size=20, line_spacing=5, text_width=50):
     new_image = image.resize([384, math.floor(total_height * scalar)])
     return new_image
 
+
 if __name__ == "__main__":
-    absoluter_pfad = os.path.dirname(os.path.abspath(__file__))
-    font = os.getenv("cat_font", absoluter_pfad + "/MatrixSans-Regular.ttf")
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    font = os.getenv("cat_font", abspath + "/MatrixSans-Regular.ttf")
     font_size = os.getenv("cat_font_size", 20)
     font_spacing = os.getenv("cat_font_spacing", -2)
     length = os.getenv("cat_length", 60)
+    max_versuche = os.getenv("cat_max-retries", 5)
     searchtime = os.getenv("cat_search", 6)
-    tempfile = os.getenv("cat_tempfile", absoluter_pfad + "/temp.pbm")
 
     input_text = sys.stdin.read()
     image = draw_text(input_text, font, font_size, font_spacing, length)
-    image.save(tempfile)
-    result = subprocess.run([sys.executable, absoluter_pfad + "/printer.py", "-s", searchtime, tempfile], capture_output=True, text=True, cwd=absoluter_pfad)
-    print(result.stdout)
-    print(result.stderr)
-    if os.path.exists(tempfile):
-        os.remove(tempfile)
+    img_bytes = BytesIO()
+    image.save(img_bytes, format='PPM')
+    img_bytes.seek(0)
+    data = img_bytes.read()
+
+    tries = 0
+    success = False
+
+    print(f"Printing...")
+    while tries < max_versuche and not success:
+        result = subprocess.run([sys.executable, abspath + "/printer.py", "-s", str(searchtime), "-"], input=data,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=abspath)
+        if result.returncode == 0:
+            print(result.stdout.decode())
+            success = True
+        else:
+            print(f"Error on try #{tries + 1} of {max_versuche}: {result.stderr.decode()}")
+            tries += 1
+
+    if not success:
+        print(f"Print aborted after {tries} retries")
